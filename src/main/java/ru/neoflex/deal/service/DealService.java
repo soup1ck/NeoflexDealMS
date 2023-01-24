f9aa48eeacd1323367f8f942ff3fd4d994818b6a
huawei <vo.gr2002@gmail.com>
2023-01-25 02:12:52 +0300

    refactor DealService

--- src/main/java/ru/neoflex/deal/service/DealService.java
+++ src/main/java/ru/neoflex/deal/service/DealService.java
@@ -5,9 +5,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import ru.neoflex.deal.data.dto.LoanApplicationRequestDTO;
 import ru.neoflex.deal.data.dto.LoanOfferDTO;
+import ru.neoflex.deal.data.enums.ApplicationStatus;
 import ru.neoflex.deal.entity.Application;
 import ru.neoflex.deal.entity.Client;
 import ru.neoflex.deal.mapper.LoanApplicationRequestClientMapper;
+import ru.neoflex.deal.mapper.LoanOfferDTOJsonbMapper;
 import ru.neoflex.deal.repository.ApplicationRepository;
 import ru.neoflex.deal.repository.ClientRepository;
 import ru.neoflex.deal.utils.ConveyorUtils;
@@ -22,6 +24,7 @@ public class DealService {
     private final ClientRepository clientRepository;
     private final ApplicationRepository applicationRepository;
     private final LoanApplicationRequestClientMapper clientMapper;
+    private final LoanOfferDTOJsonbMapper loanOfferMapper;
     private final ConveyorUtils conveyorUtils;
 
     public List<LoanOfferDTO> handleLoanRequest(LoanApplicationRequestDTO loanApplicationRequestDTO) {
@@ -29,6 +32,7 @@ public class DealService {
         log.info(clientInDb.getClientId().toString());
         Application application = new Application();
         application.setClient(clientInDb);
+        application.setStatus(ApplicationStatus.PREAPPROVAL.toString());
         Application applicationInDb = applicationRepository.save(application);
         Long applicationId = applicationInDb.getApplicationId();
         log.info(applicationId.toString());
@@ -37,5 +41,13 @@ public class DealService {
             loanOffer.setApplicationId(applicationId);
         }
         return loanOffers;
+    }
+
+    public void updateApplication (LoanOfferDTO loanOfferDTO){
+        Long applicationId = loanOfferDTO.getApplicationId();
+        Application applicationInDb = applicationRepository.getApplicationByApplicationId(applicationId);
+        applicationInDb.setStatus(ApplicationStatus.APPROVED.toString());
+        applicationInDb.setAppliedOffer(loanOfferMapper.loanOfferDTOToJsonb(loanOfferDTO));
+        applicationRepository.save(applicationInDb);
     }
 }

dba34bef2e76a0fd6871f7ccb98aac0aae57ffe8
huawei <vo.gr2002@gmail.com>
2023-01-24 19:39:59 +0300

    create LoanApplicationRequestDTO, create LoanOfferDTO

---
+++ src/main/java/ru/neoflex/deal/data/dto/LoanApplicationRequestDTO.java
@@ -0,0 +1,22 @@
+package ru.neoflex.deal.data.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+public class LoanApplicationRequestDTO {
+
+    private BigDecimal amount;
+    private Integer term;
+    private String firstName;
+    private String lastName;
+    private String middleName;
+    private String email;
+    private LocalDate birthDate;
+    private String passportSeries;
+    private String passportNumber;
+}

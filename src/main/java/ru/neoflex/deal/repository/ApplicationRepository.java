572a694aded6dabac32e902b7a4856d523526b8d
huawei <vo.gr2002@gmail.com>
2023-01-25 02:13:56 +0300

    refactor ApplicationRepository

--- src/main/java/ru/neoflex/deal/repository/ApplicationRepository.java
+++ src/main/java/ru/neoflex/deal/repository/ApplicationRepository.java
@@ -1,11 +1,20 @@
 package ru.neoflex.deal.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import ru.neoflex.deal.data.jsonb.LoanOfferJsonb;
 import ru.neoflex.deal.entity.Application;
 
 @Repository
-public interface ApplicationRepository extends JpaRepository<Application,String> {
+public interface ApplicationRepository extends JpaRepository<Application, String> {
 
-    Application findByApplicationId(String applicationId);
+    Application findByApplicationId(Long applicationId);
+
+    Application getApplicationByApplicationId(Long applicationId);
+
+    @Query("update Application set status=:status, appliedOffer=:appliedOffer where applicationId=:id")
+    void updateApplication(@Param("status") String status, @Param("appliedOffer") LoanOfferJsonb loanOfferJsonb,
+                           @Param("id") Long applicationId);
 }

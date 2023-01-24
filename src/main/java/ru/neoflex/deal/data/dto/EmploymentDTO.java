e08bc21ea1a359f7fca53bc00a0627d5be6275f6
huawei <vo.gr2002@gmail.com>
2023-01-25 02:36:16 +0300

    add EmploymentPosition, EmploymentStatus, refactor EmploymentDTO

--- src/main/java/ru/neoflex/deal/data/dto/EmploymentDTO.java
+++ src/main/java/ru/neoflex/deal/data/dto/EmploymentDTO.java
@@ -2,6 +2,8 @@ package ru.neoflex.deal.data.dto;
 
 import lombok.Getter;
 import lombok.Setter;
+import ru.neoflex.deal.data.enums.EmploymentPosition;
+import ru.neoflex.deal.data.enums.EmploymentStatus;
 
 import java.math.BigDecimal;
 

63af5468e5881aac7683eba85489dfaf00658da0
huawei <vo.gr2002@gmail.com>
2023-01-24 19:36:12 +0300

    create refactor DealController, add FeignClient, add OpenClient dependency in pom, add ConveyorUtils

--- src/main/java/ru/neoflex/deal/controller/DealController.java
+++ src/main/java/ru/neoflex/deal/controller/DealController.java
@@ -1,25 +1,25 @@
 package ru.neoflex.deal.controller;
 
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import ru.neoflex.deal.entity.Client;
-import ru.neoflex.deal.repository.ApplicationRepository;
-import ru.neoflex.deal.repository.ClientRepository;
+import ru.neoflex.deal.data.dto.LoanApplicationRequestDTO;
+import ru.neoflex.deal.data.dto.LoanOfferDTO;
+import ru.neoflex.deal.service.DealService;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping(value = "/deal")
 public class DealController {
 
-    private final ApplicationRepository applicationRepository;
-    private final ClientRepository clientRepository;
+    private final DealService dealService;
 
     @PostMapping(value = "/application")
-    public void insertClient(@RequestBody Client client) {
-        clientRepository.save(client);
+    public List<LoanOfferDTO> insertClient(@RequestBody LoanApplicationRequestDTO loanApplicationRequestDTO) {
+        return dealService.handleLoanRequest(loanApplicationRequestDTO);
     }
 }
---
+++ src/main/java/ru/neoflex/deal/utils/ConveyorUtils.java
@@ -0,0 +1,16 @@
+package ru.neoflex.deal.utils;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import ru.neoflex.deal.data.dto.LoanApplicationRequestDTO;
+import ru.neoflex.deal.data.dto.LoanOfferDTO;
+
+import java.util.List;
+
+@FeignClient(value = "deal", url = "http://localhost:8080/conveyor")
+public interface ConveyorUtils {
+
+    @PostMapping(value = "/offers")
+    List<LoanOfferDTO> getLoanOffers(@RequestBody LoanApplicationRequestDTO loanApplicationRequestDTO);
+}
